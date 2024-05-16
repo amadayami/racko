b3e5c7bd222ff5075d086dbc66b55db183b66098
amadayami <amadayamichael@gmail.com>
2024-05-16 16:58:35 -0400

    Moves the fonts to a folder. Adds values to the cards.

--- guiscript.js
+++ guiscript.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("rackoCanvas");
 const ctx = canvas.getContext("2d");
-let w = window.innerWidth;
-let h = window.innerHeight;
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 ctx.canvas.width = window.innerWidth * 0.9;
 ctx.canvas.height = window.innerHeight;
@@ -13,6 +13,12 @@ function drawCard(posx, posy, width, height){
 	ctx.strokeStyle = "black";
 	ctx.strokeRect(posx, posy, width, height);
 }
+function drawValue(posx, posy, value){
+	ctx.font = "28px Atkinson-Bold";
+	ctx.fillStyle = "black";
+	ctx.fillText(value, posx, posy);
+}
+
 let cardBaseWidth = canvas.width/12;
 let cardBaseHeight = cardBaseWidth*1.5;
 drawCard(canvas.width/2-100-5, 10, cardBaseWidth, cardBaseHeight);
@@ -22,11 +28,13 @@ drawCard(canvas.width/2 + 5, 10, cardBaseWidth, cardBaseHeight);
 for(let i = 1; i <=10; i++){
 	let posx = cardBaseWidth * i;
 	ctx.beginPath();
-	drawCard(posx, 160, cardBaseWidth, cardBaseHeight);
+	drawCard(posx, 165, cardBaseWidth, cardBaseHeight);
+	drawValue(posx+10, 195, i);
 }
 //draws second row for double racko
 for(let i = 1; i <=10; i++){
 	let posx = cardBaseWidth * i;
 	ctx.beginPath();
-	drawCard(posx, 160+cardBaseHeight, cardBaseWidth, cardBaseHeight);
+	drawCard(posx, 165+cardBaseHeight+10, cardBaseWidth, cardBaseHeight);
+	drawValue(posx+10, 195+cardBaseHeight+10, i);
 }
