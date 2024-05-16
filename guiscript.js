c823749add8bca79115ee059702cc88088bba7fe
amadayami <amadayamichael@gmail.com>
2024-05-16 15:17:05 -0400

    Adds HTML canvas. Adds script for canvas.

---
+++ guiscript.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("rackoCanvas");
+const ctx = canvas.getContext("2d");
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+ctx.canvas.width = window.innerWidth * 0.9;
+ctx.canvas.height = window.innerHeight * 0.8;
+
