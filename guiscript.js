9dad7f23ab1f59662735df03e818eb539b983063
amadayami <amadayamichael@gmail.com>
2024-05-20 17:59:14 -0400

    Refactors cards to use Card class.

--- guiscript.js
+++ guiscript.js
@@ -2,39 +2,73 @@ const canvas = document.getElementById("rackoCanvas");
 const ctx = canvas.getContext("2d");
 var w = window.innerWidth;
 var h = window.innerHeight;
+var cards = [];
+var drawCard;
+var discardCard;
 
 ctx.canvas.width = window.innerWidth * 0.9;
 ctx.canvas.height = window.innerHeight;
 
-function drawCard(posx, posy, width, height){
-	ctx.fillStyle = "white";
-	ctx.fillRect(posx, posy, width, height);
-	ctx.lineWidth = 2;
-	ctx.strokeStyle = "black";
-	ctx.strokeRect(posx, posy, width, height);
+var cardBaseWidth = canvas.width/12;
+var cardBaseHeight = cardBaseWidth*1.5;
+
+class Card{
+	constructor(x,y,w,h){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.selected = false;
+	}
+	create(){
+		const cell = new Path2D();
+		cell.rect(this.x, this.y, this.w, this.h);
+		
+		ctx.fillStyle = "white";
+		ctx.fill(cell);
+		
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 2;
+		ctx.stroke(cell);
+	}
+	select(){
+		this.selected = true;
+	}
 }
+
 function drawValue(posx, posy, value){
 	ctx.font = "28px Atkinson-Bold";
 	ctx.fillStyle = "black";
 	ctx.fillText(value, posx, posy);
 }
 
-let cardBaseWidth = canvas.width/12;
-let cardBaseHeight = cardBaseWidth*1.5;
-drawCard(canvas.width/2-100-5, 10, cardBaseWidth, cardBaseHeight);
-drawCard(canvas.width/2 + 5, 10, cardBaseWidth, cardBaseHeight);
-
 //draws first row for standard racko
-for(let i = 1; i <=10; i++){
-	let posx = cardBaseWidth * i;
-	ctx.beginPath();
-	drawCard(posx, 165, cardBaseWidth, cardBaseHeight);
-	drawValue(posx+10, 195, i);
+function setup(mode){
+	let posx;
+	for(let i = 1; i <= 10; i++){
+		posx = cardBaseWidth * i;
+		//ctx.beginPath() //not sure if i need this still
+		cards.push(new Card(posx, 165, cardBaseWidth, cardBaseHeight));
+	}
+	if(mode === "double"){
+		for(let i = 1; i <= 10; i++){
+			posx = cardBaseWidth * i;
+			//ctx.beginPath();
+			cards.push(new Card(posx, 165+cardBaseHeight+10, cardBaseWidth, cardBaseHeight));
+		}
+	}
 }
-//draws second row for double racko
-for(let i = 1; i <=10; i++){
-	let posx = cardBaseWidth * i;
-	ctx.beginPath();
-	drawCard(posx, 165+cardBaseHeight+10, cardBaseWidth, cardBaseHeight);
-	drawValue(posx+10, 195+cardBaseHeight+10, i);
+
+function drawCards(){
+	for(card of cards){
+		card.create();
+	}
 }
+
+drawCard = new Card(canvas.width/2-100-5, 10, cardBaseWidth, cardBaseHeight);
+discardCard = new Card(canvas.width/2 + 5, 10, cardBaseWidth, cardBaseHeight);
+drawCard.create();
+discardCard.create();
+setup("double");
+drawCards();
+
